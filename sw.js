77da0985a1ef14611d530d995dbe608c044d4db7
dnmmxagency-bit <dnm.mxagency@gmail.com>
2026-08-17 16:35:56 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    IMPORTANTE: sube el número de versión (v1 -> v2 -> ...) cada vez
    que cambies el código, para forzar que se limpie el caché viejo.
    ============================================================ */
-const CACHE_NAME = "dnm-agency-v7";
+const CACHE_NAME = "dnm-agency-v8";
 
 const CORE_ASSETS = [
   "./",
@@ -16,6 +16,7 @@ const CORE_ASSETS = [
   "./app.js",
   "./manifest.json",
   "./icons/isotipo.png",
+  "./icons/isotipo-white.png",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
   "./icons/favicon-64.png",
